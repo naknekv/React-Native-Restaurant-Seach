04897a4a98081fef2428f559f4fa660209033877
Ignacio Iacovino <nacho.iacovino@gmail.com>
2019-12-11 17:16:33 +0100

    Add request to Yelp API

--- App.js
+++ App.js
@@ -2,12 +2,6 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import SearchScreen from './src/screens/SearchScreen'
 
-// Client ID
-// 1v-Vkcl5y0_hRwS5hJvmwQ
-
-// API Key
-// E3OyzbZpHsEAkvFPUenBOYWfef6nUdk9K6MLqHIk5C-XIJEqRNjvk0GFA9kljBKVM86m-I_OasXgntaqy1rlot1JFX1mPvL5dIgin5y_8T1-wNdx31BUeMTYE_DwXXYx
-
 const navigator = createStackNavigator(
   {
     Search: SearchScreen
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
 import { View, Text } from 'react-native'
 import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState("")
+    const [restaurants, setRestaurants] = useState([])
+
+    const onTermSubmit = async () => {
+        const res = await yelp.get('/search', {
+            params: {
+                term,
+                limit: 50,
+                location: "san jose"
+            }
+        })
+        setRestaurants(res.data.businesses)
+    }
+
+    console.log(restaurants)
+
     return (
         <View>
             <SearchBar term={term} setTerm={setTerm} onTermSubmit={onTermSubmit} />
             <Text>
-                {term}
+                We have found {restaurants.length} results
             </Text>
         </View>
     )
